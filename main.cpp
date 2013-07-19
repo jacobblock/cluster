d81ffd1be35b3d036b81658b6eba5079e5ad2229
Jacob Block <Jacob.Block@gmail.com>
2013-07-19 13:32:25 -0400

    Accept parameter k for clusters.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <initializer_list>
+#include <cstdio>
+#include <string>
 #include "CImg/CImg.h"
 
 using namespace cimg_library;
 
-int main(int argc, const char* argv[]) {
+int main(int argc, char* argv[]) {
+
+	if(3 != argc) {
+		std::cout << "Program should be run as \"cluster -k[clusters] -i[input file]\"." << std::endl;
+		return 0;
+	}
+
+	unsigned int k = 0;
+	if(1 != std::sscanf(argv[1],"-k%u", &k)) {
+		std::cout << "k argument should be an unsigned integer" << std::endl;
+		return -1;
+	} else {
+		std::cout << "Number of clusters (k): " << std::to_string(k) << std::endl;
+	}
+
+//	std::string inputfile(argv[2]);
+//	std::cout << "Input file path: " << inputfile << std::endl;
 
     return 0;
 }
