d3e68b9edaef53a5f2da19dcd8e1cbd9d2c671df
Jacob Block <Jacob.Block@gmail.com>
2013-07-31 09:26:07 -0400

    Removing CImg. Will be using opencv library instead.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
-#include "CImg/CImg.h"
 #include <opencv2/opencv.hpp>
 
-using namespace cimg_library;
 
 int main(int argc, char* argv[]) {
 
-    cv::Mat image;
-    image = cv::imread("lena.jpg");
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Display Image", image);
-    cv::waitKey(0);
 
     if(3 != argc) {
         std::cout << "Program should be run as \"cluster -k[clusters] -i[input file]\"." << std::endl;
@@ -29,7 +22,6 @@ int main(int argc, char* argv[]) {
 
     //	std::string inputfile(argv[2]);
     //	std::cout << "Input file path: " << inputfile << std::endl;
-
 
     return 0;
 }
