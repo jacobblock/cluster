17d5d235e2336fda13f92c5254186df0b953d980
Jacob Block <Jacob.Block@gmail.com>
2013-07-31 09:20:15 -0400

    Testing opencv demo.

--- main.cpp
+++ main.cpp
@@ -2,26 +2,34 @@
 #include <cstdio>
 #include <string>
 #include "CImg/CImg.h"
+#include <opencv2/opencv.hpp>
 
 using namespace cimg_library;
 
 int main(int argc, char* argv[]) {
 
-	if(3 != argc) {
-		std::cout << "Program should be run as \"cluster -k[clusters] -i[input file]\"." << std::endl;
-		return 0;
-	}
+    cv::Mat image;
+    image = cv::imread("lena.jpg");
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
 
-	unsigned int k = 0;
-	if(1 != std::sscanf(argv[1],"-k%u", &k)) {
-		std::cout << "k argument should be an unsigned integer" << std::endl;
-		return -1;
-	} else {
-		std::cout << "Number of clusters (k): " << std::to_string(k) << std::endl;
-	}
+    if(3 != argc) {
+        std::cout << "Program should be run as \"cluster -k[clusters] -i[input file]\"." << std::endl;
+        return -1;
+    }
 
-//	std::string inputfile(argv[2]);
-//	std::cout << "Input file path: " << inputfile << std::endl;
+    unsigned int k = 0;
+    if(1 != std::sscanf(argv[1],"-k%u", &k)) {
+        std::cout << "k argument should be an unsigned integer" << std::endl;
+        return -1;
+    } else {
+        std::cout << "Number of clusters (k): " << std::to_string(k) << std::endl;
+    }
+
+    //	std::string inputfile(argv[2]);
+    //	std::cout << "Input file path: " << inputfile << std::endl;
+
 
     return 0;
 }
