fdf77927267d7e3a055ccf09b3e46278b31e5447
Jacob Block <Jacob.Block@gmail.com>
2013-08-06 09:11:58 -0500

    Reading in some arguments.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <regex>
+#include <memory>
+#include <fstream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 static double w = 100;
 
+
+
 int main(int argc, char* argv[]) {
-
-
+    // Check for correct number of arguments
     if(3 != argc) {
         std::cout << "Program should be run as \"cluster -k[clusters] -i[input file]\"." << std::endl;
         return -1;
     }
+
+    // Parse cluster value
+/*    unsigned int k = 0;
+    if(std::sscanf(argv[1],"-k%u", &k)) {
+        std::cout << "Number of clusters (k): " << std::to_string(k) << std::endl;
+    } else {
+        std::cout << "k argument should be an unsigned integer: \"-k<unsinged int>\"" << std::endl;
+        return -1;
+    }*/
+
+    
+    /*if(std::sscanf(argv[1], "-i%s", &filepath)) {
+        
+    } else {
+        std::cout << "i argument should be a filepath: \"-i[filepath]\"" << std::endl;
+        return -1;
+    }*/
 
     unsigned int k = 0;
     if(1 != std::sscanf(argv[1],"-k%u", &k)) {
@@ -20,6 +41,12 @@ int main(int argc, char* argv[]) {
         return -1;
     } else {
         std::cout << "Number of clusters (k): " << std::to_string(k) << std::endl;
+    }
+    std::ifstream input(filepath);
+
+    if(!input.is_open()) {
+        std::cout << "Couldn't open file " << filepath << std::endl;
+        return -1;
     }
     char window[] = "Clustering";
     cv::Mat image = cv::Mat::zeros(100, 100, CV_8UC3);
