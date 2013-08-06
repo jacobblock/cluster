24818e053a26a157f1473bd1c94f97d7a15e6fb1
Jacob Block <Jacob.Block@gmail.com>
2013-08-06 09:09:43 -0500

    opencv demo window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
+static double w = 100;
 
 int main(int argc, char* argv[]) {
 
@@ -19,6 +21,15 @@ int main(int argc, char* argv[]) {
     } else {
         std::cout << "Number of clusters (k): " << std::to_string(k) << std::endl;
     }
+    char window[] = "Clustering";
+    cv::Mat image = cv::Mat::zeros(100, 100, CV_8UC3);
+    MyEllipse(image, 90);
+    MyEllipse(image, 0);
+    MyEllipse(image, 45);
+    MyEllipse(image, -45);
+
+    cv::imshow(window, image);
+    cv::waitKey(0);
 
     //	std::string inputfile(argv[2]);
     //	std::cout << "Input file path: " << inputfile << std::endl;
