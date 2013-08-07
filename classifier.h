05d8f54b1c90f790950c94069fb180eae33887b1
Jacob Block <Jacob.Block@gmail.com>
2013-08-06 19:51:46 -0500

    Cleaning up headers.

--- classifier.h
+++ classifier.h
@@ -1,19 +1,19 @@
 #ifndef CLASSIFIER_H
 #define CLASSIFIER_H
 
-#include <string>
 #include <list>
 #include <vector>
 #include <opencv2/core/core.hpp>
 
 class Classifier {
-    public:
-        unsigned int k;
-        std::list<cv::Vec2i> data;
-        virtual void start() = 0;
+public:
+    unsigned int k;
+    std::list<cv::Vec2i> data;
+    virtual void start() = 0;
 };
 
 class KMeans : public Classifier {
+public:
     std::vector<cv::Vec2i> means;
 
     void start();
@@ -25,7 +25,7 @@ class KMeans : public Classifier {
 
     //std::vector<cv::Vec2i> means = init(k, data);
     //std::vector<std::vector<cv::Vec2i>> clusters = assign(means, data);
-    
+
 };
 
 #endif
--- parser.h
+++ parser.h
@@ -6,17 +6,17 @@
 #include <opencv2/core/core.hpp>
 
 class Parser {
-    public:
-        Parser(std::string filepath);
-        virtual std::list<cv::Vec2i> parse() = 0;
+public:
+    Parser(std::string filepath);
+    virtual std::list<cv::Vec2i> parse() = 0;
 
-        std::string filepath = "";
+    std::string filepath = "";
 };
 
 class ParseByCSVPoints : public Parser {
-    public:
-        ParseByCSVPoints(std::string filepath);
-        std::list<cv::Vec2i> parse();
+public:
+    ParseByCSVPoints(std::string filepath);
+    std::list<cv::Vec2i> parse();
 };
 
 #endif
